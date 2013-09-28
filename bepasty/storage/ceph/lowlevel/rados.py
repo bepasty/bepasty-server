5489a5fd4dbc03036523c7017d650efa3252286a
Bastian Blank <bastian@waldi.eu.org>
2013-09-28 21:51:27 +0200

    Finish ceph rados interface

--- bepasty/storage/ceph/lowlevel/rados.py
+++ bepasty/storage/ceph/lowlevel/rados.py
@@ -1,7 +1,9 @@
+import collections
+
 from ctypes import (
         CDLL,
         c_char_p, c_void_p, c_int, c_uint64,
-        pointer, POINTER,
+        POINTER,
         )
 
 from . import errcheck
@@ -32,6 +34,17 @@ _rados_shutdown = _librados.rados_conf_read_file
 _rados_shutdown.restype = None
 _rados_shutdown.argtypes = c_void_p,
 
+# int rados_ioctx_create(rados_t cluster, const char *pool_name, rados_ioctx_t *ioctx);
+_rados_ioctx_create = _librados.rados_ioctx_create
+_rados_ioctx_create.restype = c_int
+_rados_ioctx_create.errcheck = errcheck
+_rados_ioctx_create.argtypes = c_void_p, c_char_p, POINTER(c_void_p)
+
+# void rados_ioctx_destroy(rados_ioctx_t io);
+_rados_ioctx_destroy = _librados.rados_ioctx_destroy
+_rados_ioctx_destroy.restype = None
+_rados_ioctx_destroy.argtypes = c_void_p,
+
 
 class Rados(object):
     def __init__(self, cluster_name=None, client_name=None, config_file=None):
@@ -39,31 +52,67 @@ class Rados(object):
         self.client_name = client_name or 'client.admin'
         self.config_file = config_file
 
-        self.__context = c_void_p()
+        self._context = c_void_p()
 
     def __enter__(self):
-        if self.__context:
+        if self._context:
             raise RuntimeError
 
-        _rados_create2(self.__context, self.cluster_name, self.client_name, 0)
+        _rados_create2(self._context, self.cluster_name, self.client_name, 0)
 
         if self.config_file:
-            _rados_conf_read_file(self.__context, self.config_file)
+            _rados_conf_read_file(self._context, self.config_file)
 
-        _rados_connect(self.__context)
+        _rados_connect(self._context)
 
-        return RadosCtx(self.__context)
+        return _RadosManager(self._context)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if not self.__context:
+        if not self._context:
             raise RuntimeError
 
-        _rados_shutdown(self.__context)
+        _rados_shutdown(self._context)
 
-        self.__context = c_void_p()
+        self._context = c_void_p()
 
 
-class RadosCtx(object):
+class _RadosManager(object):
     def __init__(self, context):
-        self.__context = context
+        self._context = context
 
+    def __getitem__(self, key):
+        return RadosIoctx(self, key)
+
+    def __delitem__(self, key):
+        raise NotImplementedError
+
+    def create_pool(self, key):
+        raise NotImplementedError
+
+
+class RadosIoctx(object):
+    def __init__(self, rados, pool_name):
+        self._context, self.pool_name = rados._context, pool_name
+
+        self._io_context = c_void_p()
+
+    def __enter__(self):
+        if self._io_context:
+            raise RuntimeError
+
+        _rados_ioctx_create(self._context, self.pool_name, self._io_context)
+
+        return _RadosIoctxManager(self._io_context)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not self._io_context:
+            raise RuntimeError
+
+        _rados_ioctx_destroy(self._io_context)
+
+        self._io_context = c_void_p()
+
+
+class _RadosIoctxManager(object):
+    def __init__(self, io_context):
+        self._io_context = io_context
