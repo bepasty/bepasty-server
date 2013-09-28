bb43d350dbeac14c448148d0afab25a097bb7632
Bastian Blank <bastian@waldi.eu.org>
2013-09-28 17:32:31 +0200

    Add extra JSON endpoint for upload

--- bepasty/views/upload.py
+++ bepasty/views/upload.py
@@ -1,15 +1,13 @@
-from flask import current_app, redirect, request, url_for
+from flask import current_app, jsonify, redirect, request, url_for
 from flask.views import MethodView
 
 from ..utils.name import ItemName
 from . import blueprint
 
 
-class UploadView(MethodView):
-    def get(self):
-        raise NotImplementedError
-
-    def post(self):
+class Upload(object):
+    @staticmethod
+    def upload():
         f = request.files['file']
         if not f:
             raise NotImplementedError
@@ -28,8 +26,22 @@ class UploadView(MethodView):
 
             # Save filename
             item.meta['filename'] = f.filename
+            item.meta['size'] = offset
 
+            return n, {'filename': f.filename, 'size': offset, 'url': '/' + n}
+
+
+class UploadView(MethodView):
+    def post(self):
+        n, info = Upload.upload()
         return redirect(url_for('bepasty.display', name=n))
 
 
+class UploadViewJson(MethodView):
+    def post(self):
+        n, info = Upload.upload()
+        return jsonify({'files': [info]})
+
+
 blueprint.add_url_rule('/+upload', view_func=UploadView.as_view('upload'))
+blueprint.add_url_rule('/+upload/json', view_func=UploadViewJson.as_view('upload_json'))
