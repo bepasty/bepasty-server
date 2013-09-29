f3b41d30d304c6a564fbb1b1c9709c879e7669c5
Bastian Blank <bastian@waldi.eu.org>
2013-09-29 14:00:45 +0200

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bepasty',
+    version='dev',
+    packages=find_packages(),
+    package_data={
+        'bepasty': [
+            'static/app/css/*.css',
+            'static/app/js/*.js',
+            'static/bootstrap/fonts/*',
+            'static/jquery.fileupload/css/*.css',
+            'static/jquery.fileupload/img/*',
+            'static/jquery.fileupload/js/*.js',
+            'static/jquery.fileupload/js/vendor/*.js',
+            'templates/*',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask>=0.10',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
