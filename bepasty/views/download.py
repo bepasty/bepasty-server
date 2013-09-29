9650300cb82d840f3ce7679c624a138f96070ba2
Bastian Blank <bastian@waldi.eu.org>
2013-09-29 19:50:56 +0200

    Rename baseitem to item

--- bepasty/views/download.py
+++ bepasty/views/download.py
@@ -14,31 +14,31 @@ from . import blueprint
 class DownloadView(MethodView):
     def get(self, name):
         try:
-            baseitem = current_app.storage.open(name)
+            item = current_app.storage.open(name)
         except OSError as e:
             if e.errno == errno.ENOENT:
                 raise NotFound()
 
-        if not baseitem.meta['complete']:
+        if not item.meta['complete']:
             try:
                 error = 'Upload incomplete. Try again later.'
-                return render_template('display_error.html', name=name, item=baseitem, error=error), 409
+                return render_template('display_error.html', name=name, item=item, error=error), 409
             finally:
-                baseitem.close()
+                item.close()
 
         def stream():
-            with baseitem as item:
+            with item as _item:
                 # Stream content from storage
                 offset = 0
-                size = item.data.size
+                size = _item.data.size
                 while offset < size:
-                    buf = item.data.read(16*1024, offset)
+                    buf = _item.data.read(16*1024, offset)
                     offset += len(buf)
                     yield buf
 
         ret = Response(stream_with_context(stream()))
-        ret.headers['Content-Disposition'] = 'attachment; filename="{}"'.format(baseitem.meta['filename'])
-        ret.headers['Content-Length'] = baseitem.meta['size']
+        ret.headers['Content-Disposition'] = 'attachment; filename="{}"'.format(item.meta['filename'])
+        ret.headers['Content-Length'] = item.meta['size']
         return ret
 
 
