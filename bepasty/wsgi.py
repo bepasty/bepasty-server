77364312f0481c1131d64669cc0c6204c2a60193
Bastian Blank <bastian@waldi.eu.org>
2013-09-29 23:40:15 +0200

    Add wsgi module

---
+++ bepasty/wsgi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: 2013 Bastian Blank <bastian@waldi.eu.org>
+# License: BSD 2-clause, see LICENSE for details.
+
+from . import create_app
+
+application = create_app()
+
+if __name__ == '__main__':
+    application.run(debug=True)
