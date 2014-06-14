17e19d29165f0b976334b07f89b7594fd0503d5c
Thomas Waldmann <tw@waldmann-edv.de>
2014-06-14 02:24:52 +0200

    add purge function to purge by age, size and/or type
    give '*' as name to process all items in the storage

--- bepasty/cli/object.py
+++ bepasty/cli/object.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import time
 
 from flask import Flask
 
@@ -12,30 +13,60 @@ class Main(object):
     argparser.add_argument('config', metavar='CONFIG')
     argparser.add_argument('names', metavar='NAME', nargs='+')
 
-    def do_info(self, name, item, args):
-        print name
-        for key, value in sorted(item.meta.items()):
-            print '  ', key, value
+    def do_purge(self, storage, name, args):
+        with storage.open(name) as item:
+            file_name = item.meta['filename']
+            file_size = item.meta['size']
+            t_upload = item.meta['timestamp']
+            file_type = item.meta['type']
+        purge = True
+        if args.purge_age is not None:
+            dt = args.purge_age * 24 * 3600  # age in days
+            tnow = time.time()
+            purge = purge and t_upload < tnow - dt
+        if args.purge_size is not None:
+            max_size = args.purge_size * 1024 * 1024  # size in MiB
+            purge = purge and file_size > max_size
+        if args.purge_type is not None:
+            purge = purge and file_type.startswith(args.purge_type)
+        if purge:
+            print 'removing: %s (%s %dB %s)' % (name, file_name, file_size, file_type)
+            if not args.purge_dry_run:
+                storage.remove(name)
+
+    _parser = _subparsers.add_parser('purge', help='Purge objects')
+    _parser.set_defaults(func=do_purge)
+    _parser.add_argument('-S', '--size', dest='purge_size', type=int, default=None)
+    _parser.add_argument('-A', '--age', dest='purge_age', type=int, default=None)
+    _parser.add_argument('-T', '--type', dest='purge_type', default=None)
+    _parser.add_argument('-D', '--dry-run', dest='purge_dry_run', action='store_true')
+
+    def do_info(self, storage, name, args):
+        with storage.open(name) as item:
+            print name
+            for key, value in sorted(item.meta.items()):
+                print '  ', key, value
 
     _parser = _subparsers.add_parser('info', help='Display information about objects')
     _parser.set_defaults(func=do_info)
 
-    def do_set(self, name, item, args):
-        print name
+    def do_set(self, storage, name, args):
+        with storage.openwrite(name) as item:
+            print name
 
-        if args.flag_complete is not None:
-            if args.flag_complete:
-                print '  set complete'
-            else:
-                print '  set not complete'
-            item.meta['complete'] = args.flag_complete
+            if args.flag_complete is not None:
+                if args.flag_complete:
+                    print '  set complete'
+                else:
+                    print '  set not complete'
+                item.meta['complete'] = args.flag_complete
 
-        if args.flag_unlocked is not None:
-            if args.flag_unlocked:
-                print '  set unlocked'
-            else:
-                print '  set not unlocked'
-            item.meta['unlocked'] = args.flag_unlocked
+            if args.flag_unlocked is not None:
+                if args.flag_unlocked:
+                    print '  set unlocked'
+                else:
+                    print '  set not unlocked'
+                item.meta['unlocked'] = args.flag_unlocked
 
     _parser = _subparsers.add_parser('set', help='Set flags on objects')
     _parser.set_defaults(func=do_set)
@@ -61,10 +92,13 @@ class Main(object):
             for i in app.before_request_funcs.get(None, ()):
                 i()
 
-            for name in args.names:
+            if len(args.names) == 1 and args.names[0] == '*':
+                names = list(storage)
+            else:
+                names = args.names
+            for name in names:
                 try:
-                    with storage.openwrite(name) as item:
-                        args.func(self, name, item, args)
+                    args.func(self, storage, name, args)
                 except Exception:
                     logging.exception('Failed to handle item %s', name)
 
