67669c1af4056741602567cee547a73e03faa6c7
Thomas Waldmann <tw@waldmann-edv.de>
2014-06-14 17:06:43 +0200

    add consistency command to check hash/size consistency, update missing hashes, fix wrong size/hash

--- bepasty/cli/object.py
+++ bepasty/cli/object.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import time
+import hashlib
 
 from flask import Flask
 
@@ -62,6 +63,61 @@ class Main(object):
                          help='only remove if file size > PURGE_SIZE MiB')
     _parser.add_argument('-T', '--type', dest='purge_type', default=None,
                          help='only remove if file mimetype starts with PURGE_TYPE')
+
+    def do_consistency(self, storage, name, args):
+        with storage.openwrite(name) as item:
+            file_name = item.meta['filename']
+            meta_size = item.meta['size']
+            meta_type = item.meta['type']
+            t_upload = item.meta['timestamp-upload']
+            meta_hash = item.meta['hash']
+
+            print 'checking: %s (%s %dB %s)' % (name, file_name, meta_size, meta_type)
+
+            size = item.data.size
+            size_consistent = size == meta_size
+            if not size_consistent:
+                print "Inconsistent file size: meta: %d file: %d" % (meta_size, size)
+                if args.consistency_fix:
+                    print "Writing computed file size into metadata..."
+                    item.meta['size'] = size
+                    size_consistent = True
+
+            hasher = hashlib.sha256()
+            offset = 0
+            while offset < size:
+                buf = item.data.read(1024 * 1024, offset)
+                offset += len(buf)
+                hasher.update(buf)
+            file_hash = hasher.hexdigest()
+
+            hash_consistent = meta_hash == file_hash
+            if not hash_consistent:
+                if meta_hash:
+                    print "Inconsistent hashes:"
+                    print "meta: %s" % meta_hash
+                    print "file: %s" % file_hash
+                else:
+                    # the upload code can not compute hashes for chunked uploads and thus stores an empty hash.
+                    # we can fix that empty hash with the computed hash from the file we have in storage.
+                    print "Empty hash in metadata."
+                if args.consistency_fix or args.consistency_compute and not meta_hash:
+                    print "Writing computed file hash into metadata..."
+                    item.meta['hash'] = file_hash
+                    hash_consistent = True
+
+        if args.consistency_remove and not (size_consistent and hash_consistent):
+            print 'REMOVING inconsistent file!'
+            storage.remove(name)
+
+    _parser = _subparsers.add_parser('consistency', help='Consistency-related functions')
+    _parser.set_defaults(func=do_consistency)
+    _parser.add_argument('-C', '--compute', dest='consistency_compute', action='store_true',
+                         help='compute missing hashes and write into metadata')
+    _parser.add_argument('-F', '--fix', dest='consistency_fix', action='store_true',
+                         help='write computed hash/size into metadata')
+    _parser.add_argument('-R', '--remove', dest='consistency_remove', action='store_true',
+                         help='remove files with inconsistent hash/size')
 
     def do_info(self, storage, name, args):
         with storage.open(name) as item:
