b0298d9761b6fb4724a94860afdc70060bfdd3aa
Thomas Waldmann <tw@waldmann-edv.de>
2014-06-29 21:33:18 +0200

    locked item: render normally for admin (and offer unlocking), do not render contents / offer download for non-admins
    also: fix download button, so it actually works

--- bepasty/views/display.py
+++ bepasty/views/display.py
@@ -29,16 +29,16 @@ class DisplayView(MethodView):
             raise
 
         with item as item:
-            if not item.meta.get('unlocked'):
-                error = 'File locked.'
-            elif not item.meta.get('complete'):
+            if not item.meta.get('complete'):
                 error = 'Upload incomplete. Try again later.'
             else:
                 error = None
             if error:
                 return render_template('display_error.html', name=name, item=item, error=error), 409
             ct = item.meta['type']
-            if ct.startswith('text/x-bepasty-'):
+            if not item.meta.get('unlocked') and not may(ADMIN):
+                rendered_content = u"item is locked"
+            elif ct.startswith('text/x-bepasty-'):
                 # special bepasty items
                 if ct == 'text/x-bepasty-list':
                     names = item.data.read(item.data.size, 0).splitlines()
