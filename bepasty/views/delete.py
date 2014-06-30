5b1eefb315cd9094de8c8827e0f3a8c0eeefe95a
Thomas Waldmann <tw@waldmann-edv.de>
2014-06-30 07:01:17 +0200

    delete view: make sure item is closed before it is removed from storage

--- bepasty/views/delete.py
+++ bepasty/views/delete.py
@@ -16,30 +16,24 @@ class DeleteView(MethodView):
         if not may(DELETE):
             abort(403)
         try:
-            item = current_app.storage.open(name)
+            with current_app.storage.open(name) as item:
+                if item.meta.get('locked'):
+                    error = 'File locked.'
+                elif not item.meta.get('complete'):
+                    error = 'Upload incomplete. Try again later.'
+                else:
+                    error = None
+                if error:
+                    return render_template('display_error.html', name=name, item=item, error=error), 409
+
+            current_app.storage.remove(name)
+
         except (OSError, IOError) as e:
             if e.errno == errno.ENOENT:
                 return render_template('file_not_found.html'), 404
             raise
 
-        if item.meta.get('locked'):
-            error = 'File locked.'
-        elif not item.meta.get('complete'):
-            error = 'Upload incomplete. Try again later.'
-        else:
-            error = None
-        if error:
-            try:
-                return render_template('display_error.html', name=name, item=item, error=error), 409
-            finally:
-                item.close()
-
-        try:
-            item = current_app.storage.remove(name)
-        except (OSError, IOError) as e:
-            if e.errno == errno.ENOENT:
-                return render_template('file_not_found.html'), 404
-
         return redirect(url_for('bepasty.display', name=name))
+
 
 blueprint.add_url_rule('/<itemname:name>/+delete', view_func=DeleteView.as_view('delete'))
