4ba24019e6c855988fb96bf99925f14a62ff9fc1
Thomas Waldmann <tw@waldmann-edv.de>
2014-07-26 18:23:34 +0200

    xstatic view: cleanup / minor changes

--- bepasty/views/xstatic.py
+++ bepasty/views/xstatic.py
@@ -2,20 +2,21 @@ from ..bepasty_xstatic import serve_files
 
 from flask import Response, abort
 from flask import send_from_directory
+from werkzeug.exceptions import NotFound
 
 from . import blueprint
+
 
 @blueprint.route('/xstatic/<name>', defaults=dict(filename=''))
 @blueprint.route('/xstatic/<name>/<path:filename>')
 def xstatic(name, filename):
-    '''Route to serve the xstatic files (from serve_files)'''
-    print 'YEAHHHH > xstatic > name=%s filename=%s' % (name, filename)
+    """Route to serve the xstatic files (from serve_files)"""
     try:
         base_path = serve_files[name]
     except KeyError:
-        abort(404)
+        raise NotFound()
 
     if not filename:
-        abort(404)
+        raise NotFound()
 
     return send_from_directory(base_path, filename)
