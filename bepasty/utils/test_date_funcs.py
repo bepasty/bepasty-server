f0ad34dbd45363a622adffcc4188c61b0b03ae66
Valentin Pratz <vp.pratz@yahoo.de>
2014-07-27 15:44:00 +0200

    Added functional tests with selenium for the expiration date feature

---
+++ bepasty/utils/test_date_funcs.py
@@ -0,0 +1,26 @@
+# Copyright: 2014 Valentin Pratz <vp.pratz@yahoo.de>
+# License: BSD 2-clause, see LICENSE for details.
+
+from ..utils.date_funcs import time_unit_to_sec
+import selenium
+
+
+def test_unit_to_secs():
+    """
+    Test all units
+    """
+    cases = {
+        'MINUTES': 60,
+        'HOURS': 60 * 60,
+        'DAYS': 60 * 60 * 24,
+        'WEEKS': 60 * 60 * 24 * 7,
+        'MONTHS': 60 * 60 * 24 * 30,
+        'YEARS': 60 * 60 * 24 * 365,
+        'FOREVER': -1
+    }
+    for key, value in cases.items():
+        yield compute_1_unit_to_sec, key, value
+
+
+def compute_1_unit_to_sec(unit, expectation):
+    assert time_unit_to_sec(1, unit) == expectation
