f3c955773eaff89bfd9bdfdf8ce96f0f46f8d09f
Thomas Waldmann <tw@waldmann-edv.de>
2014-08-10 01:38:40 +0200

    pep8 fix

--- bepasty/apis/rest.py
+++ bepasty/apis/rest.py
@@ -191,4 +191,3 @@ blueprint.add_url_rule('/rest', view_func=InfoView.as_view('api_info'))
 blueprint.add_url_rule('/rest/items', view_func=ItemUploadView.as_view('items'))
 blueprint.add_url_rule('/rest/items/<itemname:name>', view_func=ItemDetailView.as_view('items_detail'))
 blueprint.add_url_rule('/rest/items/<itemname:name>/download', view_func=ItemDownloadView.as_view('items_download'))
-
