492f197ba66dd85941a92341da9aca1f35bcc635
Thomas Waldmann <tw@waldmann-edv.de>
2014-09-06 20:33:30 +0200

    implement own random id generation to get shorter and easy-to-read IDs

--- bepasty/utils/name.py
+++ bepasty/utils/name.py
@@ -1,25 +1,89 @@
 # Copyright: 2013 Bastian Blank <bastian@waldi.eu.org>
 # License: BSD 2-clause, see LICENSE for details.
 
-import uuid
+import re
+import random
 
 from werkzeug.routing import BaseConverter
 
+ID_LENGTH = 8
+
+letters_lower = set("abcdefghijklmnopqrstuvwxyz")
+letters_upper = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+digits = set("0123456789")
+
+# this stuff might be hard to read / differentiate or is otherwise unwanted:
+unwanted = set("1lI"
+               "0O"
+               "8B"
+               "5S"
+               '+'  # used in URL dispatching for views, e.g. +login
+               '/'  # used in URLs and fs paths
+              )
+
+all_chars = letters_lower | letters_upper | digits
+all_chars -= unwanted
+all_chars = ''.join(sorted(all_chars))
+
+
+def encode(x, length, alphabet=all_chars):
+    """
+    encode x in alphabet (with "leading zeroes")
+
+    :param x: integer number
+    :param length: length of return sequence
+    :return: sequence of alphabet members [list]
+    """
+    if x < 0:
+        raise ValueError("negative values are not supported")
+    if length < 0:
+        raise ValueError("length must be >= 0")
+    n = len(alphabet)
+    if n < 2:
+        raise ValueError("alphabet must be at least 2 elements long")
+    code = []
+    counter = length
+    while x > 0 and counter > 0:
+        x, r = divmod(x, n)
+        code.append(alphabet[r])
+        counter -= 1
+    leading_zeroes = length - len(code)
+    code += list(alphabet[0] * leading_zeroes)
+    return list(reversed(code))
+
+
+def make_id(length, x=None, alphabet=all_chars):
+    """
+    generate a id of <length> from value <x>.
+    if x is None, use a random value for x.
+    for the id, use elements from alphabet.
+    """
+    base = len(alphabet)  # e.g. 10
+    if x is None:
+        x = random.randint(0,                  # 000 (length==3) ...
+                           base ** length - 1  # 999 (length==3)
+                          )
+    return ''.join(encode(x, length, alphabet))
+
 
 class ItemName(str):
-    def __new__(cls, uuid):
-        return str(uuid)
+    def __new__(cls, uid):
+        return str(uid)
 
     @classmethod
-    def create(cls):
-        return cls(uuid.uuid4())
+    def create(cls, length=ID_LENGTH):
+        return cls(make_id(length))
 
 
 class ItemNameConverter(BaseConverter):
     """
-    Accept UUID like names.
+    Accept the names of the style as we generate.
     """
-    regex = '[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}'
+    # for a while, support both old uuid4-style as well as new shorter IDs
+    regex = ('([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})'
+             '|'
+             '([%(valid)s]{%(length)d})'
+            ) % dict(valid=re.escape(all_chars), length=ID_LENGTH)
     weight = 200
 
 
--- bepasty/utils/test_name.py
+++ bepasty/utils/test_name.py
@@ -1,9 +1,79 @@
 # Copyright: 2013 Bastian Blank <bastian@waldi.eu.org>
 # License: BSD 2-clause, see LICENSE for details.
 
-from .name import ItemName
+import pytest
+
+from .name import ItemName, encode, make_id
 
 
 def test_create():
     n = ItemName.create()
     assert n
+
+
+def test_make_id_type():
+    assert isinstance(make_id(2), str)
+
+
+def test_make_id_length():
+    for length in xrange(10):
+        assert len(make_id(length)) == length
+
+
+def test_make_id_alphabet():
+    # id must contain alphabet chars ONLY
+    assert set(make_id(10, alphabet="abc")) <= set(['a', 'b', 'c'])
+
+
+def test_make_id_unique():
+    length, count = 6, 10000
+    ids = {make_id(length) for i in xrange(count)}
+    # if we did not encounter duplicates, set size must be <count>
+    # of course, in extremely rare cases, this test might fail
+    assert len(ids) == count
+
+
+def test_encode_length():
+    length = 42
+    assert len(encode(12345, length)) == length
+
+
+def test_encode_binary():
+    assert encode(0, 0, "01") == []  # zero length
+    assert encode(1, 0, "01") == []  # zero length
+    assert encode(0, 1, "01") == ['0']  # length match
+    assert encode(1, 1, "01") == ['1']  # length match
+    assert encode(0, 2, "01") == ['0', '0']  # leading zeroes
+    assert encode(1, 2, "01") == ['0', '1']  # leading zeroes
+    assert encode(2, 2, "01") == ['1', '0']  # length match
+    assert encode(3, 2, "01") == ['1', '1']  # length match
+    assert encode(4, 2, "01") == ['0', '0']  # overflow truncated
+
+
+def test_encode_special():
+    # equivalent to binary, but we see the code is rather flexible
+    assert encode(0, 2, ".+") == ['.', '.']
+    assert encode(1, 2, ".+") == ['.', '+']
+    assert encode(2, 2, ".+") == ['+', '.']
+    assert encode(3, 2, ".+") == ['+', '+']
+
+
+def test_encode_decimal():
+    assert encode(123, 3, "0123456789") == ['1', '2', '3']  # length match
+    assert encode(456, 4, "0123456789") == ['0', '4', '5', '6']  # leading zeroes
+    assert encode(7890, 3, "0123456789") == ['8', '9', '0']  # overflow truncated
+
+
+def test_encode_hex():
+    assert encode(31, 2, "0123456789ABCDEF") == ['1', 'F']
+
+
+def test_encode_errors():
+    with pytest.raises(ValueError):  # ValueError: alphabet must be at least 2 elements long
+        encode(1, 1, "0")
+
+    with pytest.raises(ValueError):  # ValueError: length must be >= 0
+        encode(1, -1, "0")
+
+    with pytest.raises(ValueError):  # ValueError: negative values are not supported
+        encode(-1, 1, "0")
