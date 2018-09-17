5f0ecafa1455818bab04f7173602bd584de37a76
Thomas Waldmann <tw@waldmann-edv.de>
2018-09-17 11:00:32 +0200

    pytest: yield-based tests are deprecated

--- bepasty/tests/test_date_funcs.py
+++ bepasty/tests/test_date_funcs.py
@@ -1,19 +1,16 @@
+import pytest
+
 from bepasty.utils.date_funcs import time_unit_to_sec, FOREVER
 
 
-def test_unit_to_secs():
-    cases = {
-        'MINUTES': 60,
-        'HOURS': 60 * 60,
-        'DAYS': 60 * 60 * 24,
-        'WEEKS': 60 * 60 * 24 * 7,
-        'MONTHS': 60 * 60 * 24 * 30,
-        'YEARS': 60 * 60 * 24 * 365,
-        'FOREVER': FOREVER
-    }
-    for key, value in cases.items():
-        yield compute_1_unit_to_sec, key, value
-
-
-def compute_1_unit_to_sec(unit, expectation):
+@pytest.mark.parametrize('unit,expectation', [
+    ('MINUTES', 60),
+    ('HOURS', 60 * 60),
+    ('DAYS', 60 * 60 * 24),
+    ('WEEKS', 60 * 60 * 24 * 7),
+    ('MONTHS', 60 * 60 * 24 * 30),
+    ('YEARS', 60 * 60 * 24 * 365),
+    ('FOREVER', FOREVER),
+])
+def test_unit_to_secs(unit, expectation):
     assert time_unit_to_sec(1, unit) == expectation
