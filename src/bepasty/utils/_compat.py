283110b349891ce12f0728d4a4fda09b59363acb
Thomas Waldmann <tw@waldmann-edv.de>
2020-11-14 04:23:18 +0100

    drop py2 support from _compat.py

--- src/bepasty/utils/_compat.py
+++ src/bepasty/utils/_compat.py
@@ -1,20 +1,8 @@
-import sys
-
 __all__ = ['urljoin', 'urlparse']
 
-PY2 = sys.version_info[0] == 2
+from urllib.parse import urlparse, urljoin
 
-if PY2:
-    from urlparse import urlparse, urljoin
-
-    range_type = xrange  # noqa: F821
-    bytes_type = str
-    iteritems = lambda d: d.iteritems()  # noqa: E731
-    from collections import MutableMapping  # noqa: F401
-else:
-    from urllib.parse import urlparse, urljoin
-
-    range_type = range
-    bytes_type = bytes
-    iteritems = lambda d: iter(d.items())  # noqa: E731
-    from collections.abc import MutableMapping  # noqa: F401
+range_type = range
+bytes_type = bytes
+iteritems = lambda d: iter(d.items())  # noqa: E731
+from collections.abc import MutableMapping  # noqa: F401
