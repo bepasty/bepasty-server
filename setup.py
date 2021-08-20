58f33e7391ec9fa90f7b32562c27fc6842ad8473
August Feng <au.fengster@gmail.com>
2021-08-19 22:39:20 -0400

    Revert removal of setuptools_scm

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 from setuptools import setup
-setup()
+
+setup(
+    use_scm_version={
+        'write_to': 'src/bepasty/_version.py',
+    },
+)
--- src/bepasty/__init__.py
+++
@@ -1,5 +0,0 @@
-VERSION = (1, 0, 0)
-
-__version__ = "{major}.{minor}.{micro}".format(
-            major=VERSION[0], minor=VERSION[1], micro=VERSION[2]
-)
